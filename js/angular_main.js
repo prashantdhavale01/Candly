942a0d9d38eee69cf7532434e9acd2133142f611
prashantboot <prashant.d@bootinfotech.com>
2019-08-09 18:57:40 +0530

    updated

---
+++ js/angular_main.js
@@ -0,0 +1,106 @@
+var Menu = [{
+    "id": 1,
+    "name": "Home",
+    "level": 0,
+    "parentID": 0,
+    "href": "#",
+    "liClass": "has-submenu"
+  },
+  {
+    "id": 2,
+    "name": "Pages",
+    "level": 0,
+    "parentID": 0,
+    "href": "#",
+    "liClass": "has-submenu"
+  }, {
+    "id": 3,
+    "name": "Elements",
+    "level": 0,
+    "parentID": 0,
+    "href": "#",
+    "liClass": "has-submenu"
+  }, {
+    "id": 4,
+    "name": "Work",
+    "level": 0,
+    "parentID": 0,
+    "href": "#",
+    "liClass": "has-submenu"
+  }, {
+    "id": 5,
+    "name": "Blog",
+    "level": 0,
+    "parentID": 0,
+    "href": "#",
+    "liClass": "has-submenu"
+  }, {
+    "id": 6,
+    "name": "Shop",
+    "level": 0,
+    "parentID": 0,
+    "href": "#",
+    "liClass": "has-submenu"
+  },
+  {
+    "id": 7,
+    "name": "Home Classic",
+    "level": 1,
+    "parentID": 1,
+    "href": "#",
+    "liClass": ""
+  },
+  {
+    "id": 8,
+    "name": "Home Corporate",
+    "level": 1,
+    "parentID": 1,
+    "href": "index-corporate.html",
+    "liClass": ""
+  },
+  {
+    "id": 9,
+    "name": "Home Agency",
+    "level": 1,
+    "parentID": 1,
+    "href": "index-agency.html",
+    "liClass": ""
+  },
+  {
+    "id": 10,
+    "name": "Home Portfolio",
+    "level": 1,
+    "parentID": 1,
+    "href": "index-portfolio.html",
+    "liClass": ""
+  },
+  {
+    "id": 11,
+    "name": "Home Landing",
+    "level": 1,
+    "parentID": 1,
+    "href": "index-landing.html",
+    "liClass": ""
+  },
+  {
+    "id": 12,
+    "name": "Home Countdown",
+    "level": 1,
+    "parentID": 1,
+    "href": "index-countdown.html",
+    "liClass": ""
+  },
+  {
+    "id": 13,
+    "name": "Home Countdown",
+    "level": 1,
+    "parentID": 1,
+    "href": "index-countdown.html",
+    "liClass": ""
+  }
+];
+
+var app = angular.module("homePage", []);
+app.controller("menuControl", function ($scope) {
+  $scope.menu = Menu;
+});
