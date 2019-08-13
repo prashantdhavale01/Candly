b078f86300d7ba1ca0552a1132e37782f3f7fb2e
prashantboot <prashant.d@bootinfotech.com>
2019-08-13 14:15:41 +0530

    updated header menu

--- js/angular_main.js
+++ js/angular_main.js
@@ -3,6 +3,7 @@ var Menu = [{
     "name": "Home",
     "level": 0,
     "parentID": 0,
+    "hasChild": 1,
     "href": "#",
     "liClass": "has-submenu"
   },
@@ -11,6 +12,7 @@ var Menu = [{
     "name": "Pages",
     "level": 0,
     "parentID": 0,
+    "hasChild": 1,
     "href": "#",
     "liClass": "has-submenu"
   }, {
@@ -18,6 +20,7 @@ var Menu = [{
     "name": "Elements",
     "level": 0,
     "parentID": 0,
+    "hasChild": 1,
     "href": "#",
     "liClass": "has-submenu"
   }, {
@@ -25,6 +28,7 @@ var Menu = [{
     "name": "Work",
     "level": 0,
     "parentID": 0,
+    "hasChild": 1,
     "href": "#",
     "liClass": "has-submenu"
   }, {
@@ -32,6 +36,7 @@ var Menu = [{
     "name": "Blog",
     "level": 0,
     "parentID": 0,
+    "hasChild": 1,
     "href": "#",
     "liClass": "has-submenu"
   }, {
@@ -39,14 +44,15 @@ var Menu = [{
     "name": "Shop",
     "level": 0,
     "parentID": 0,
+    "hasChild": 1,
     "href": "#",
     "liClass": "has-submenu"
-  },
-  {
+  }, {
     "id": 7,
     "name": "Home Classic",
     "level": 1,
     "parentID": 1,
+    "hasChild": 0,
     "href": "#",
     "liClass": ""
   },
@@ -55,6 +61,7 @@ var Menu = [{
     "name": "Home Corporate",
     "level": 1,
     "parentID": 1,
+    "hasChild": 0,
     "href": "index-corporate.html",
     "liClass": ""
   },
@@ -63,6 +70,7 @@ var Menu = [{
     "name": "Home Agency",
     "level": 1,
     "parentID": 1,
+    "hasChild": 0,
     "href": "index-agency.html",
     "liClass": ""
   },
@@ -71,6 +79,7 @@ var Menu = [{
     "name": "Home Portfolio",
     "level": 1,
     "parentID": 1,
+    "hasChild": 0,
     "href": "index-portfolio.html",
     "liClass": ""
   },
@@ -79,6 +88,7 @@ var Menu = [{
     "name": "Home Landing",
     "level": 1,
     "parentID": 1,
+    "hasChild": 0,
     "href": "index-landing.html",
     "liClass": ""
   },
@@ -87,20 +97,420 @@ var Menu = [{
     "name": "Home Countdown",
     "level": 1,
     "parentID": 1,
+    "hasChild": 0,
     "href": "index-countdown.html",
     "liClass": ""
   },
   {
     "id": 13,
-    "name": "Home Countdown",
+    "name": "About Us",
     "level": 1,
-    "parentID": 1,
-    "href": "index-countdown.html",
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-about.html",
     "liClass": ""
-  }
+  },
+  {
+    "id": 14,
+    "name": "Services",
+    "level": 1,
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-services.html",
+    "liClass": ""
+  },
+  {
+    "id": 15,
+    "name": "FAQ",
+    "level": 1,
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-faq.html",
+    "liClass": ""
+  },
+  {
+    "id": 16,
+    "name": "Pricing",
+    "level": 1,
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-pricing.html",
+    "liClass": ""
+  },
+  {
+    "id": 17,
+    "name": "Error Page",
+    "level": 1,
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-error.html",
+    "liClass": ""
+  },
+
+  {
+    "id": 18,
+    "name": "Team",
+    "level": 1,
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-team.html",
+    "liClass": ""
+  },
+
+  {
+    "id": 19,
+    "name": "Careers",
+    "level": 1,
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-careers.html",
+    "liClass": ""
+  },
+  {
+    "id": 20,
+    "name": "Clients",
+    "level": 1,
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-clients.html",
+    "liClass": ""
+  },
+  {
+    "id": 21,
+    "name": "Blank Page",
+    "level": 1,
+    "parentID": 2,
+    "hasChild": 0,
+    "href": "page-blank.html",
+    "liClass": ""
+  },
+  {
+    "id": 22,
+    "name": "Clients",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "page-clients.html",
+    "liClass": ""
+  },
+  {
+    "id": 23,
+    "name": "Animation",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-animation.html",
+    "liClass": ""
+  },
+  {
+    "id": 24,
+    "name": "Accordions",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-accordions.html",
+    "liClass": ""
+  },
+  {
+    "id": 25,
+    "name": "Alerts",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-alerts.html",
+    "liClass": ""
+  },
+  {
+    "id": 26,
+    "name": "Buttons",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-buttons.html",
+    "liClass": ""
+  },
+  {
+    "id": 27,
+    "name": "Counter Numbers",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-countnumbers.html",
+    "liClass": ""
+  },
+  {
+    "id": 28,
+    "name": "Grid",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-grid.html",
+    "liClass": ""
+  },
+  {
+    "id": 29,
+    "name": "Material Icons",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-material-icons.html",
+    "liClass": ""
+  },
+  {
+    "id": 30,
+    "name": "Etline Icons",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-etline-icon.html",
+    "liClass": ""
+  },
+  {
+    "id": 31,
+    "name": "Colored Icons",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-colored-icons.html",
+    "liClass": ""
+  },
+  {
+    "id": 32,
+    "name": "Images",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-images.html",
+    "liClass": ""
+  },
+  {
+    "id": 33,
+    "name": "Labels",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-labels.html",
+    "liClass": ""
+  },
+  {
+    "id": 34,
+    "name": "Typography",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-typography.html",
+    "liClass": ""
+  },
+  {
+    "id": 35,
+    "name": "Google Maps",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-maps.html",
+    "liClass": ""
+  },
+  {
+    "id": 36,
+    "name": "Modals",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-modals.html",
+    "liClass": ""
+  },
+  {
+    "id": 37,
+    "name": "Panels",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-panels.html",
+    "liClass": ""
+  },
+
+  {
+    "id": 38,
+    "name": "Progressbars",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-progressbars.html",
+    "liClass": ""
+  },
+  {
+    "id": 39,
+    "name": "Tables",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-tables.html",
+    "liClass": ""
+  },
+  {
+    "id": 40,
+    "name": "Responsive Video",
+    "level": 1,
+    "parentID": 3,
+    "hasChild": 0,
+    "href": "elements-video.html",
+    "liClass": ""
+  },
+  {
+    "id": 41,
+    "name": "Boxed",
+    "level": 1,
+    "parentID": 4,
+    "hasChild": 0,
+    "href": "portfolio-boxed.html",
+    "liClass": ""
+  },
+  {
+    "id": 42,
+    "name": "Wide",
+    "level": 1,
+    "parentID": 4,
+    "hasChild": 0,
+    "href": "portfolio-wide.html",
+    "liClass": ""
+  },
+  {
+    "id": 43,
+    "name": "Masonry",
+    "level": 1,
+    "parentID": 4,
+    "hasChild": 0,
+    "href": "portfolio-masonry.html",
+    "liClass": ""
+  },
+  {
+    "id": 44,
+    "name": "Masonry Wide",
+    "level": 1,
+    "parentID": 4,
+    "hasChild": 0,
+    "href": "portfolio-masonry-wide.html",
+    "liClass": ""
+  },
+  {
+    "id": 45,
+    "name": "Single Work",
+    "level": 1,
+    "parentID": 4,
+    "hasChild": 0,
+    "href": "portfolio-single.html",
+    "liClass": ""
+  },
+  {
+    "id": 46,
+    "name": "Promo Page",
+    "level": 1,
+    "parentID": 4,
+    "hasChild": 0,
+    "href": "portfolio-promo.html",
+    "liClass": ""
+  },
+  {
+    "id": 47,
+    "name": "Classic",
+    "level": 1,
+    "parentID": 5,
+    "hasChild": 0,
+    "href": "blog-classic.html",
+    "liClass": ""
+  },
+  {
+    "id": 48,
+    "name": "Masonry",
+    "level": 1,
+    "parentID": 5,
+    "hasChild": 0,
+    "href": "blog-masonry.html",
+    "liClass": ""
+  },
+  {
+    "id": 49,
+    "name": "Columns",
+    "level": 1,
+    "parentID": 5,
+    "hasChild": 0,
+    "href": "blog-columns.html",
+    "liClass": ""
+  },
+  {
+    "id": 50,
+    "name": "Single Post",
+    "level": 1,
+    "parentID": 5,
+    "hasChild": 0,
+    "href": "blog-single.html",
+    "liClass": ""
+  },
+  {
+    "id": 51,
+    "name": "Products",
+    "level": 1,
+    "parentID": 6,
+    "hasChild": 0,
+    "href": "shop-products.html",
+    "liCheaderMenuCommonlass": ""
+  },
+  {
+    "id": 52,
+    "name": "Single Product",
+    "level": 1,
+    "parentID": 6,
+    "hasChild": 0,
+    "href": "shop-single-product.html",
+    "liClass": ""
+  },
+  {
+    "id": 53,
+    "name": "Shopping Cart",
+    "level": 1,
+    "parentID": 6,
+    "hasChild": 0,
+    "href": "shop-shopping-cart.html",
+    "liClass": ""
+  },
+  {
+    "id": 54,
+    "name": "Checkout",
+    "level": 1,
+    "parentID": 6,
+    "hasChild": 0,
+    "href": "shop-checkout.html",
+    "liClass": ""
+  },
+  {
+    "id": 55,
+    "name": "Wishlist",
+    "level": 1,
+    "parentID": 6,
+    "hasChild": 0,
+    "href": "shop-wishlist.html",
+    "liClass": ""
+  },
 ];
 
 var app = angular.module("homePage", []);
-app.controller("menuControl", function ($scope) {
+var menuControl = function ($scope) {
   $scope.menu = Menu;
-});
+}
+var header = function () {
+  return {
+    restrict: "C",
+    templateUrl: "header.html",
+    scope: true
+  };
+}
+var footer = function () {
+  return {
+    restrict: "C",
+    templateUrl: "footer.html",
+    scope: true
+  };
+}
+app.controller("menuControl", menuControl);
+app.directive("headerMenuCommon", header);
+app.directive("footerMenuCommon", footer);
