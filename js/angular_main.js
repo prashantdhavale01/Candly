a597d65ea9f3ffb5d11d41ef8a6440217ae0e372
prashantboot <prashant.d@bootinfotech.com>
2019-08-19 19:45:54 +0530

    updated code

--- js/angular_main.js
+++ js/angular_main.js
@@ -502,9 +502,527 @@ var Menu = [{
   },
 ];
 
+var MenuHn = [{
+  "id": 1,
+  "name": "होम",
+  "level": 0,
+  "parentID": 0,
+  "hasChild": 1,
+  "href": "#",
+  "liClass": "has-submenu"
+},
+{
+  "id": 2,
+  "name": "पेज",
+  "level": 0,
+  "parentID": 0,
+  "hasChild": 1,
+  "href": "#",
+  "liClass": "has-submenu"
+}, {
+  "id": 3,
+  "name": "एलीमेंट्स",
+  "level": 0,
+  "parentID": 0,
+  "hasChild": 1,
+  "href": "#",
+  "liClass": "has-submenu"
+}, {
+  "id": 4,
+  "name": "वर्क",
+  "level": 0,
+  "parentID": 0,
+  "hasChild": 1,
+  "href": "#",
+  "liClass": "has-submenu"
+}, {
+  "id": 5,
+  "name": "ब्लॉग",
+  "level": 0,
+  "parentID": 0,
+  "hasChild": 1,
+  "href": "#",
+  "liClass": "has-submenu"
+}, {
+  "id": 6,
+  "name": "शॉप",
+  "level": 0,
+  "parentID": 0,
+  "hasChild": 1,
+  "href": "#",
+  "liClass": "has-submenu"
+}, {
+  "id": 100,
+  "name": "कॉंटॅक्ट",
+  "level": 0,
+  "parentID": 0,
+  "hasChild": 0,
+  "href": "contact.html",
+  "liClass": "has-submenu"
+},
+ {
+  "id": 7,
+  "name": "Home Classic",
+  "level": 1,
+  "parentID": 1,
+  "hasChild": 0,
+  "href": "#",
+  "liClass": ""
+},
+{
+  "id": 8,
+  "name": "Home Corporate",
+  "level": 1,
+  "parentID": 1,
+  "hasChild": 0,
+  "href": "index-corporate.html",
+  "liClass": ""
+},
+{
+  "id": 9,
+  "name": "Home Agency",
+  "level": 1,
+  "parentID": 1,
+  "hasChild": 0,
+  "href": "index-agency.html",
+  "liClass": ""
+},
+{
+  "id": 10,
+  "name": "Home Portfolio",
+  "level": 1,
+  "parentID": 1,
+  "hasChild": 0,
+  "href": "index-portfolio.html",
+  "liClass": ""
+},
+{
+  "id": 11,
+  "name": "Home Landing",
+  "level": 1,
+  "parentID": 1,
+  "hasChild": 0,
+  "href": "index-landing.html",
+  "liClass": ""
+},
+{
+  "id": 12,
+  "name": "Home Countdown",
+  "level": 1,
+  "parentID": 1,
+  "hasChild": 0,
+  "href": "index-countdown.html",
+  "liClass": ""
+},
+{
+  "id": 13,
+  "name": "About Us",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-about.html",
+  "liClass": ""
+},
+{
+  "id": 14,
+  "name": "Services",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-services.html",
+  "liClass": ""
+},
+{
+  "id": 15,
+  "name": "FAQ",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-faq.html",
+  "liClass": ""
+},
+{
+  "id": 16,
+  "name": "Pricing",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-pricing.html",
+  "liClass": ""
+},
+{
+  "id": 17,
+  "name": "Error Page",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-error.html",
+  "liClass": ""
+},
+
+{
+  "id": 18,
+  "name": "Team",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-team.html",
+  "liClass": ""
+},
+
+{
+  "id": 19,
+  "name": "Careers",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-careers.html",
+  "liClass": ""
+},
+{
+  "id": 20,
+  "name": "Clients",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-clients.html",
+  "liClass": ""
+},
+{
+  "id": 21,
+  "name": "Blank Page",
+  "level": 1,
+  "parentID": 2,
+  "hasChild": 0,
+  "href": "page-blank.html",
+  "liClass": ""
+},
+{
+  "id": 22,
+  "name": "Clients",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "page-clients.html",
+  "liClass": ""
+},
+{
+  "id": 23,
+  "name": "Animation",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-animation.html",
+  "liClass": ""
+},
+{
+  "id": 24,
+  "name": "Accordions",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-accordions.html",
+  "liClass": ""
+},
+{
+  "id": 25,
+  "name": "Alerts",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-alerts.html",
+  "liClass": ""
+},
+{
+  "id": 26,
+  "name": "Buttons",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-buttons.html",
+  "liClass": ""
+},
+{
+  "id": 27,
+  "name": "Counter Numbers",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-countnumbers.html",
+  "liClass": ""
+},
+{
+  "id": 28,
+  "name": "Grid",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-grid.html",
+  "liClass": ""
+},
+{
+  "id": 29,
+  "name": "Material Icons",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-material-icons.html",
+  "liClass": ""
+},
+{
+  "id": 30,
+  "name": "Etline Icons",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-etline-icon.html",
+  "liClass": ""
+},
+{
+  "id": 31,
+  "name": "Colored Icons",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-colored-icons.html",
+  "liClass": ""
+},
+{
+  "id": 32,
+  "name": "Images",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-images.html",
+  "liClass": ""
+},
+{
+  "id": 33,
+  "name": "Labels",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-labels.html",
+  "liClass": ""
+},
+{
+  "id": 34,
+  "name": "Typography",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-typography.html",
+  "liClass": ""
+},
+{
+  "id": 35,
+  "name": "Google Maps",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-maps.html",
+  "liClass": ""
+},
+{
+  "id": 36,
+  "name": "Modals",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-modals.html",
+  "liClass": ""
+},
+{
+  "id": 37,
+  "name": "Panels",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-panels.html",
+  "liClass": ""
+},
+
+{
+  "id": 38,
+  "name": "Progressbars",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-progressbars.html",
+  "liClass": ""
+},
+{
+  "id": 39,
+  "name": "Tables",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-tables.html",
+  "liClass": ""
+},
+{
+  "id": 40,
+  "name": "Responsive Video",
+  "level": 1,
+  "parentID": 3,
+  "hasChild": 0,
+  "href": "elements-video.html",
+  "liClass": ""
+},
+{
+  "id": 41,
+  "name": "Boxed",
+  "level": 1,
+  "parentID": 4,
+  "hasChild": 0,
+  "href": "portfolio-boxed.html",
+  "liClass": ""
+},
+{
+  "id": 42,
+  "name": "Wide",
+  "level": 1,
+  "parentID": 4,
+  "hasChild": 0,
+  "href": "portfolio-wide.html",
+  "liClass": ""
+},
+{
+  "id": 43,
+  "name": "Masonry",
+  "level": 1,
+  "parentID": 4,
+  "hasChild": 0,
+  "href": "portfolio-masonry.html",
+  "liClass": ""
+},
+{
+  "id": 44,
+  "name": "Masonry Wide",
+  "level": 1,
+  "parentID": 4,
+  "hasChild": 0,
+  "href": "portfolio-masonry-wide.html",
+  "liClass": ""
+},
+{
+  "id": 45,
+  "name": "Single Work",
+  "level": 1,
+  "parentID": 4,
+  "hasChild": 0,
+  "href": "portfolio-single.html",
+  "liClass": ""
+},
+{
+  "id": 46,
+  "name": "Promo Page",
+  "level": 1,
+  "parentID": 4,
+  "hasChild": 0,
+  "href": "portfolio-promo.html",
+  "liClass": ""
+},
+{
+  "id": 47,
+  "name": "Classic",
+  "level": 1,
+  "parentID": 5,
+  "hasChild": 0,
+  "href": "blog-classic.html",
+  "liClass": ""
+},
+{
+  "id": 48,
+  "name": "Masonry",
+  "level": 1,
+  "parentID": 5,
+  "hasChild": 0,
+  "href": "blog-masonry.html",
+  "liClass": ""
+},
+{
+  "id": 49,
+  "name": "Columns",
+  "level": 1,
+  "parentID": 5,
+  "hasChild": 0,
+  "href": "blog-columns.html",
+  "liClass": ""
+},
+{
+  "id": 50,
+  "name": "Single Post",
+  "level": 1,
+  "parentID": 5,
+  "hasChild": 0,
+  "href": "blog-single.html",
+  "liClass": ""
+},
+{
+  "id": 51,
+  "name": "Products",
+  "level": 1,
+  "parentID": 6,
+  "hasChild": 0,
+  "href": "shop-products.html",
+  "liCheaderMenuCommonlass": ""
+},
+{
+  "id": 52,
+  "name": "Single Product",
+  "level": 1,
+  "parentID": 6,
+  "hasChild": 0,
+  "href": "shop-single-product.html",
+  "liClass": ""
+},
+{
+  "id": 53,
+  "name": "Shopping Cart",
+  "level": 1,
+  "parentID": 6,
+  "hasChild": 0,
+  "href": "shop-shopping-cart.html",
+  "liClass": ""
+},
+{
+  "id": 54,
+  "name": "Checkout",
+  "level": 1,
+  "parentID": 6,
+  "hasChild": 0,
+  "href": "shop-checkout.html",
+  "liClass": ""
+},
+{
+  "id": 55,
+  "name": "Wishlist",
+  "level": 1,
+  "parentID": 6,
+  "hasChild": 0,
+  "href": "shop-wishlist.html",
+  "liClass": ""
+},
+];
+
+
+
 var app = angular.module("candllyApp", []);
 var menuControl = function ($scope) {
   $scope.menu = Menu;
+  $scope.ln = "En";
+
+  $scope.languageChange = function(){
+    if($scope.ln == "En"){
+      $scope.menu = MenuHn;
+      $scope.ln = "Hn"
+    }
+    else{
+      $scope.menu = Menu;
+      $scope.ln = "En"
+    }
+  } 
 }
 var header = function () {
   return {
@@ -513,13 +1031,21 @@ var header = function () {
     scope: true
   };
 }
-var parallax = function () {
+var footer = function () {
   return {
     restrict: "C",
     templateUrl: "footer.html",
     scope: true
   };
 }
+var parallax = function () {
+  return {
+    restrict: "C",
+    templateUrl: "parallax.html",
+    scope: true
+  };
+}
+
 app.controller("menuControl", menuControl);
 app.directive("headerMenuCommon", header);
 app.directive("footerMenuCommon", footer);
